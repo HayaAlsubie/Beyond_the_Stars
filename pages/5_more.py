b912071713928503a04b00441743f32fca7515f1
HayaAlsubie <HayaAjab1@gmail.com>
2025-04-28 16:57:45 +0300

    new structure

--- pages/5_more.py
+++ pages/5_more.py
@@ -3,7 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# لو تحتاجين تحميل الداتا مرة ثانية
 @st.cache_data
 def load_data():
     return pd.read_csv("notebooks/review_data.csv")
